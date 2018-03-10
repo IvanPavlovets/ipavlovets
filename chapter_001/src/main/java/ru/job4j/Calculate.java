192b7e2721dcc1743964b705a652b5a9c0afcc8a
IvanPavlovets <ivan150287@gmail.com>
2018-03-10 15:34:56 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,22 @@
-package main.java.job4j;
+package main.java.ru.job4j;
 
+/**
+* Calculate. Этот класс содержит только метод main,
+* который вызывает статический метод println() для вывода
+* аргумента, в виде строки, в консоль
+*
+* @author Ivan Pavlovets (ivan150287@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
 public class Calculate{
+
+	/**
+	* Main. Метод являеться точкой входа программы,
+        	* в его теле вызываются другие методы.
+	*
+	* @param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world.");
 	}
