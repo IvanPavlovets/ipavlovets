bfc45f888f716a75b78cc813588dd6fad2b4caa2
IvanPavlovets <ivan150287@gmail.com>
2018-03-29 20:35:06 +0700

    4.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,42 @@
+package ru.job4j.loop;
+
+/**
+ * Класс строит шахаматную доску.
+ *
+ * @author Ivan Pavlovets (ivan150287@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+
+    /**
+     * Метод рисует шахмотную доску из символов X и пробелов.
+     *
+     * @param width - ширина доски.
+     * @param height - высота доски.
+     * @return строку изображающую шахматную доску в псевдографике.
+     */
+    public String paint(int width, int height){
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // условие проверки, что писать пробел или X
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board.paint(5,4));
+    }
+
+}
