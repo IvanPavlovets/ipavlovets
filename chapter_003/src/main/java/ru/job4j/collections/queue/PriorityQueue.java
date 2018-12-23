d1593d10fdafe6c180e79d7f85eb31088713105d
IvanPavlovets <ivan150287@gmail.com>
2018-12-24 00:09:46 +0700

    2. Очередь с приоритетом на LinkedList

--- chapter_003/src/main/java/ru/job4j/collections/queue/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/collections/queue/PriorityQueue.java
@@ -11,34 +11,25 @@ public class PriorityQueue {
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определять по полю приоритет.
-     *
+     * <p>
      * Сначала происходит вставка первого элемента,
      * если первый элемент имеет больший приоретет то в цикле пройтись по коллекции
      * и вставить элемент туда где его приоритет будет больше элемента коллекции или вставить элеммент в конец
      * если у элемента самый низкий приорет.
-     *
+     * <p>
      * Для вставик использовать add(int index, E value)
      *
      * @param task задача
      */
     public void put(Task task) {
         int i = 0;
-        if (tasks.size() != 0) {
-            if (tasks.getFirst().getPriority() > task.getPriority()) {
-                tasks.addFirst(task);
-            } else {
-                while (tasks.get(i) != tasks.getLast() && tasks.get(i).getPriority() < task.getPriority()) {
-                    i++;
-                }
-                if (tasks.get(i) == tasks.getLast()) {
-                    tasks.addLast(task);
-                } else {
-                    tasks.add(i, task);
-                }
-            }
-        } else {
-            tasks.addFirst(task);
+        while (!tasks.isEmpty() && tasks.get(i) != tasks.getLast() && tasks.get(i).getPriority() < task.getPriority()) {
+            i++;
         }
+        if (!tasks.isEmpty() && tasks.get(i).getPriority() < task.getPriority()) {
+            i++;
+        }
+        tasks.add(i, task);
     }
 
     /**
