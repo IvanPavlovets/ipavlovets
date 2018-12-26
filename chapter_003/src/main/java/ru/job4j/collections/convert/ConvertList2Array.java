af4e77331c0fc30990f71553866199d77c6b8c5b
IvanPavlovets <ivan150287@gmail.com>
2018-12-26 20:33:58 +0700

    3. Конвертация ArrayList в двухмерный массив. Исправил цикл foreach

--- chapter_003/src/main/java/ru/job4j/collections/convert/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/collections/convert/ConvertList2Array.java
@@ -10,23 +10,21 @@ public class ConvertList2Array {
      * (результат в переменной tail),
      * то оставшееся количество в массиве заполняеться нулями
      * (заполняем коллекцию на недостающее кол-во).
+     *
      * @param list
      * @return
      */
-    public int[][] toArray (List<Integer> list, int rows) {
+    public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[rows][cells];
         int out = 0;
-        int i = 0;
-        for (int[] arr : array) {
-            int in = 0;
-            for (int q : arr) {
-                array[out][in++] = list.get(i++);
-                if (i == list.size()){
-                    break;
-                }
+        int in = 0;
+        for (Integer integer : list) {
+            array[out][in++] = integer;
+            if (in == cells) {
+                out++;
+                in = 0;
             }
-            out++;
         }
         return array;
     }
