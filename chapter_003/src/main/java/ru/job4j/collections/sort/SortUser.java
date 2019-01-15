5be98514abd8ac6d6213a3790243acd625dad7f7
IvanPavlovets <ivan150287@gmail.com>
2019-01-15 23:36:00 +0700

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/collections/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/collections/sort/SortUser.java
@@ -1,15 +1,43 @@
 package ru.job4j.collections.sort;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
 
     public Set<User> sort(List<User> list) {
-        Set<User> userSet = new TreeSet<>();
-        userSet.addAll(list);
-        return userSet;
+        return new TreeSet<User>(list);
+    }
+
+    /**
+     * Метод сортировки по длине имени.
+     * @param list
+     * @return - отсортированный по имени список.
+     */
+    public List<User> sortNameLengh(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() > o2.getName().length() ? 1 : -1;
+            }
+        });
+        return list;
+    }
+
+    /**
+     * Метод сортировки по обоим полям,
+     * сначала сортировка по имени в лексикографическом порядке,
+     * потом по возрасту.
+     * @param list
+     * @return - отсортированный список.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                final int rst = o1.getName().compareTo(o2.getName());
+                return rst != 0 ? rst : Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/collections/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/collections/sort/SortUserTest.java
@@ -2,9 +2,7 @@ package ru.job4j.collections.sort;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -29,4 +27,44 @@ public class SortUserTest {
         expected.add(new User("P", 5));
         assertThat(result.toArray(), is(expected.toArray()));
     }
+
+    @Test
+    public void wnenInputListWhenReturnListSortedByNameLengh() {
+        List<User> resultList = new ArrayList<User>();
+        resultList.addAll(new SortUser().sortNameLengh(
+                Arrays.asList(new User("AA", 1),
+                        new User("BBB", 3),
+                        new User("PPPPP", 4),
+                        new User("I", 2),
+                        new User("HHHH", 5))));
+
+        List<User> expectedList = new ArrayList<User>();
+        expectedList.add(new User("I", 2));
+        expectedList.add(new User("AA", 1));
+        expectedList.add(new User("BBB", 3));
+        expectedList.add(new User("HHHH", 5));
+        expectedList.add(new User("PPPPP", 4));
+        assertThat(resultList.toArray(), is(expectedList.toArray()));
+    }
+
+
+    @Test
+    public void wnenInputListWhenReturnListSortedByNameAndByAge() {
+        List<User> userList2 = new ArrayList<User>();
+        userList2.addAll(new SortUser().sortByAllFields(
+                Arrays.asList(
+                        new User("Сергей", 25),
+                        new User("Иван", 30),
+                        new User("Сергей", 20),
+                        new User("Иван", 25))));
+
+        List<User> expectedList = new ArrayList<User>();
+        expectedList.add(new User("Иван", 25));
+        expectedList.add(new User("Иван", 30));
+        expectedList.add(new User("Сергей", 20));
+        expectedList.add(new User("Сергей", 25));
+        assertThat(userList2.toArray(), is(expectedList.toArray()));
+    }
+
+
 }
