f1f44508877e999f06ad70eaa9c78de3915ff549
IvanPavlovets <ivan150287@gmail.com>
2019-06-25 20:55:26 +0700

    3. Уникальность элементов и сортировка. [#110225]

--- chapter_004/src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -18,14 +18,13 @@ public class ProfilesTest {
         Profile profile1 = new Profile();
         Profile profile2 = new Profile();
         Profile profile3 = new Profile();
-        List<Profile> profiles = Arrays.asList(profiles1.createProfile(profile1, "A", "AA", 5, 10),
-                profiles1.createProfile(profile2, "A", "AA", 5, 12),
+        List<Profile> profiles = Arrays.asList(profiles1.createProfile(profile1, "BB", "AA", 5, 10),
+                profiles1.createProfile(profile2, "BB", "AA", 5, 10),
                 profiles1.createProfile(profile3, "A", "AA", 5, 15)
         );
         List<Profile.Address> result = profiles1.coolect(profiles);
-        List<Profile.Address> expected = Arrays.asList(profiles1.createAddress(profile1, "A", "AA", 5, 10),
-                profiles1.createAddress(profile2, "A", "AA", 5, 12),
-                profiles1.createAddress(profile3, "A", "AA", 5, 15)
+        List<Profile.Address> expected = Arrays.asList(profiles1.createAddress(profile1, "A", "AA", 5, 15),
+                profiles1.createAddress(profile2, "BB", "AA", 5, 10)
         );
         assertThat(result, is(expected));
     }
