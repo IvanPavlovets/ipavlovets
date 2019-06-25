28e2a7e549beb6dba887252b718c21902e09391a
IvanPavlovets <ivan150287@gmail.com>
2019-06-25 21:26:20 +0700

    4. Преобразование List в Map. [#110226]

--- chapter_004/src/main/java/ru/job4j/stream/Student.java
+++ chapter_004/src/main/java/ru/job4j/stream/Student.java
@@ -4,15 +4,26 @@ import java.util.Objects;
 
 public class Student {
     public int score;
+    public String surname;
 
     public Student(int score) {
         this.score = score;
+    }
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
     }
 
     @Override
     public String toString() {
         return "Student{" +
                 "score=" + score +
+                ", surname='" + surname + '\'' +
                 '}';
     }
 
@@ -21,12 +32,13 @@ public class Student {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return score == student.score;
+        return score == student.score &&
+                Objects.equals(surname, student.surname);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(score);
+        return Objects.hash(score, surname);
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,9 +2,7 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -52,4 +50,22 @@ public class SchoolTest {
         );
         assertThat(result, is(expected));
     }
+
+    /**
+     * В тетсе в цикле заполнються коллекции List и Map.
+     * Проверяеться преобразование list в Map, путем их равенсва.
+     */
+    @Test
+    public void whenCalltoMapThenGetMapOfStudent() {
+        School school = new School();
+        List<Student> students = new ArrayList<>();
+        Map<String, Student> expected = new TreeMap<String, Student>();
+        for (int i = 0; i <= 50; i += 10) {
+            students.add(new Student(i, "styd" + i));
+            expected.put("styd" + i, new Student(i, "styd" + i));
+        }
+        Map<String, Student> result = school.toMap(students);
+        assertThat(result, is(expected));
+    }
+
 }
