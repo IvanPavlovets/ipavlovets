38315166a0d67660fb7993552f0b393a6acb5112
IvanPavlovets <ivan150287@gmail.com>
2019-11-07 23:21:14 +0700

    5.3.1. Создать динамический список на базе массива.[#179568]

--- chapter_005/src/main/java/ru/job4j/collectionsPro/list/ContainerList.java
+++ chapter_005/src/main/java/ru/job4j/collectionsPro/list/ContainerList.java
@@ -64,24 +64,5 @@ public class ContainerList<E> implements Iterable<E> {
                 '}';
     }
 
-    public static void main(String[] args) {
-        ContainerList<Integer> list = new ContainerList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        list.add(11);
-
-        System.out.println(list);
-        System.out.println(list.get(2));
-    }
-
 }
 
