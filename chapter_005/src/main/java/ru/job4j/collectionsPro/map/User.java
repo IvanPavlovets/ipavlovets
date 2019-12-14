389c7739b56ff853612cd8fac5b82505a0d5802c
IvanPavlovets <ivan150287@gmail.com>
2019-12-14 15:53:48 +0700

    1. Создать модель User[#179574]

---
+++ chapter_005/src/main/java/ru/job4j/collectionsPro/map/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.collectionsPro.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, int dd, int MM, int yyyy) {
+        this.name = name;
+        this.children = children;
+        this.birthday = Calendar.getInstance();
+        this.birthday.set(yyyy, MM, dd);
+    }
+}
