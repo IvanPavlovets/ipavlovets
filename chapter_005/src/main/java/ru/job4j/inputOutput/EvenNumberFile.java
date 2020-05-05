194bd2d51e6af08a8e1ecf64f4d697e6bac81b52
IvanPavlovets <ivan150287@gmail.com>
2020-05-05 14:55:11 +0700

    0. Видео[#179607] - Задача чтение из файла и четное ли число.

---
+++ chapter_005/src/main/java/ru/job4j/inputOutput/EvenNumberFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.inputOutput;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try {
+            FileInputStream in = new FileInputStream("even.txt");
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            Integer number;
+            boolean rsl;
+            for (String line : lines) {
+                number = Integer.parseInt(line);
+                rsl = number % 2 == 0;
+                System.out.printf("%d - %b\n", number, rsl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
