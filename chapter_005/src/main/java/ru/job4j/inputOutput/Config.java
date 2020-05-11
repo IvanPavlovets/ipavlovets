868e80ab0e25a864b39c504f44b990e495e6975c
IvanPavlovets <ivan150287@gmail.com>
2020-05-11 13:49:32 +0700

    1. Читаем файл конфигурации[#179603]. .filter(line -> line.startsWith("#") && line.isBlank())                                                           .forEach(line -> {                                                               String[] tokens = string.split("=");                                                               values.put(tokens[0], tokens[1]);
                                                                  вместо for (String line : lines) {
                                                                                         addValue(line);
                                                                                     } v3

--- chapter_005/src/main/java/ru/job4j/inputOutput/Config.java
+++ chapter_005/src/main/java/ru/job4j/inputOutput/Config.java
@@ -3,10 +3,8 @@ package ru.job4j.inputOutput;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
@@ -17,22 +15,18 @@ public class Config {
     }
 
     public void load() {
+        values.clear();
         try {
             BufferedReader read = new BufferedReader(new FileReader(this.path));
             read.lines()
-                    .filter(line -> line.startsWith("#") && line.isBlank())
+                    .filter(l -> !l.startsWith("#") && !l.isBlank())
                     .forEach(line -> {
-                        String[] tokens = string.split("=");
+                        String[] tokens = line.split("=");
                         values.put(tokens[0], tokens[1]);
                     });
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void addValue(String string) {
-        String[] tokens = string.split("=");
-        values.put(tokens[0], tokens[1]);
     }
 
     public String value(String key) {
@@ -54,6 +48,5 @@ public class Config {
     public static void main(String[] args) {
         Config config = new Config("app.properties");
         config.load();
-        System.out.println(config.value("hibernate.connection.password"));
     }
 }
