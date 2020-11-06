8b05eafeb0c37d6dc9a5d07ce26bdc9a4397c724
IvanPavlovets <ivan150287@gmail.com>
2020-11-06 17:13:32 +0700

    0. Socket - ServerSocket botAnswer

--- chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
@@ -8,25 +8,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isActive = true;
+            String str;
+            String answer = null;
             while (isActive) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    String answer = null;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
-                            answer = "Shutdown!";
-                            isActive = false;
-                            server.close();
-                            break;
-                        }
-                        answer = "Hello!";
+                    str = in.readLine();
+                    System.out.println(str);
+                    String[] part1 = str.split("HTTP/");
+                    String[] part2 = part1[0].split("msg=");
+                    System.out.println(part2.length);
+                    if (part2.length == 2) {
+                        answer = part2[1];
+                    }
+                    if (str.contains("Bye")) {
+                        answer = "Shutdown!";
+                        isActive = false;
+                        server.close();
                     }
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.write(answer.getBytes());
