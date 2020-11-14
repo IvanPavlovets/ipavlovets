169c9f40fc7556e46e37fa4ecf2a5edc26a3ce96
IvanPavlovets <ivan150287@gmail.com>
2020-11-14 11:18:23 +0700

    1. Бот[7921#179615]

--- chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
@@ -6,28 +6,32 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isActive = true;
+            String str;
+            String answer = null;
             while (isActive) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    String msg = "HTTP/1.1 200 OKK\r\n\\";
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
-                            msg = "HTTP/1.1 200 Shutdown!\r\n\\";
-                            isActive = false;
-                            server.close();
-                        }
+                    str = in.readLine();
+                    System.out.println(str);
+                    String[] part1 = str.split("HTTP/");
+                    String[] part2 = part1[0].split("msg=");
+                    if (part2.length == 2) {
+                        answer = part2[1].replace("%20", " ");
                     }
-                    out.write(msg.getBytes());
+                    if (str.contains("Exit")) {
+                        answer = "Shutdown server!";
+                        isActive = false;
+                        server.close();
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                 }
             }
         }
