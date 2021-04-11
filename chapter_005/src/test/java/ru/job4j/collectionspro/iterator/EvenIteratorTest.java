4e7533c7483ff4e67bec315ab5c7c94f6be09ce1
IvanPavlovets <ivan150287@gmail.com>
2021-04-11 14:04:44 +0700

    4.5. Travis CI и PostgreSQL [#98950]

--- chapter_005/src/test/java/ru/job4j/collectionspro/iterator/EvenIteratorTest.java
+++ chapter_005/src/test/java/ru/job4j/collectionspro/iterator/EvenIteratorTest.java
@@ -1,10 +1,10 @@
 package ru.job4j.collectionspro.iterator;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -12,50 +12,52 @@ import static org.hamcrest.Matchers.is;
 
 public class EvenIteratorTest {
 
-    private Iterator<Integer> iter;
+    private Iterator<Integer> it;
 
     @Before
-    public void setUp() {
-        iter = new EvenIterator(new int[]{1, 2, 3, 4, 5, 6, 7});
+    public void setUp(){
+        it = new EvenIterator(new int[]{1, 2, 3, 4, 5, 6, 7});
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void shouldReturnEvenNumbersSequentially() {
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(2));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(4));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(6));
-        assertThat(iter.hasNext(), is(false));
-        iter.next();
+    public void shouldReturnEvenNumbersSequentially () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
     }
 
     @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(2));
-        assertThat(iter.next(), is(4));
-        assertThat(iter.next(), is(6));
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
     }
 
     @Test
-    public void shouldReturnFalseIfNoAnyEvenNumbers() {
-        iter = new EvenIterator(new int[]{1});
-        assertThat(iter.hasNext(), is(false));
+    public void  shouldReturnFalseIfNoAnyEvenNumbers(){
+        it = new EvenIterator(new int[]{1});
+        assertThat(it.hasNext(), is(false));
     }
 
     @Test
-    public void allNumbersAreEven() {
-        iter = new EvenIterator(new int[]{2, 4, 6, 8});
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(2));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(4));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(6));
-        assertThat(iter.hasNext(), is(true));
-        assertThat(iter.next(), is(8));
+    public void allNumbersAreEven(){
+        it = new EvenIterator(new int[]{2,4,6,8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
     }
 }
+
+
