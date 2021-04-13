a5452f98191780c7fda726330b3bb36fc30904c7
IvanPavlovets <ivan150287@gmail.com>
2021-04-13 15:16:55 +0700

    4. Slf4j - вывод exception. [#268853]

--- chapter_005/src/main/java/ru/job4j/db/UsageLog4j.java
+++ chapter_005/src/main/java/ru/job4j/db/UsageLog4j.java
@@ -8,6 +8,12 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
+
         String name = "Ivan Pavlovets";
         int age = 33;
         char sex = 'M';
--- chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_005/src/main/java/ru/job4j/socket/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +11,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isActive = true;
             String str;
@@ -34,6 +40,8 @@ public class EchoServer {
                     out.write(answer.getBytes());
                 }
             }
+        } catch (Exception e) {
+            LOG.error("IOException in logger:", e);
         }
     }
 }
